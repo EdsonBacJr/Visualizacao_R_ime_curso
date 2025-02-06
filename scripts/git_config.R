41aff84973131373b20a959f246b0f6ae0d488e5
EdsonBacJr <edsonbaccarojr@gmail.com>
2025-02-06 16:01:13 -0300

    mais exemplo e arquivo index.html

---
+++ scripts/git_config.R
@@ -0,0 +1,11 @@
+library(usethis)
+
+# Verifica o status do projeto e configurações de acesso
+git_sitrep()
+
+# Inicia o projeto de git. Roda uma vez só
+# use_git()
+
+# Cria o repositório no github subindo pela primeira vez. Roda uma vez só.
+# usethis::use_github(private = TRUE)
+
